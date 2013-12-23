d3c156e96854b416e55dc9fa300353f251a7c5e7
Alan Salganik <asalganik1@gmail.com>
2013-12-23 13:21:45 -0600

    Made changes to Omniauth code

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '561659067248763', 'e1a4c5f44c0e3aa765347cc55fdb5c6a', :display => "popup"
+  provider :facebook, 'CODE', 'CODE', :display => "popup"
 end
